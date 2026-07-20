8317a9e7d7c79d7f3788686a346d7ce4e2c1e289
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-20 09:19:27 +0200

    design user interface and add model file
    Designed User Interface in Main.storyboard
    
    Added the HousePrices.mlmodel to the project after training it with CreateML.

--- PricePrediction/ViewController.swift
+++ PricePrediction/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var stackView: UIStackView!
+    
+    @IBOutlet var numberOfRoos: UISegmentedControl!
+    @IBOutlet var numberOfBathrooms: UISegmentedControl!
+    @IBOutlet var garageCapacity: UISegmentedControl!
+    @IBOutlet var condition: UISegmentedControl!
+    
+    @IBOutlet var yearBuiltLabel: UILabel!
+    @IBOutlet var yearBuiltSlider: UISlider!
+    
+    @IBOutlet var sizeLabel: UILabel!
+    @IBOutlet var sizeSlider: UISlider!
+    
+    @IBOutlet var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func updatePrediction(_ sender: Any) {
+        
+    }
+    
 }
-
